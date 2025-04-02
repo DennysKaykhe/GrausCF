d7d2b18d27da43aaab50de9773e8d3bf1ecb485c
Dennys Kaykhe Justino de Assis <Dennyssenpai@gmail.com>
2025-04-02 10:13:34 -0300

    Eu amo graus

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿double c;
+double f;
+
+Console.Clear();
+
+Console.Write ("Digite sua temperatura em °C = ");
+c = Convert.ToDouble(Console.ReadLine());
+
+f = c * 1.8 + 32;
+
+Console.WriteLine($"{c}ºC equivalente a {f}ºF");
